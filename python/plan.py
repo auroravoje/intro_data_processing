bb05daa5ca1b67c321728bf9442731934ed6aad2
Geir Arne Hjelle <geirarne@gmail.com>
2022-10-27 16:56:05 +0200

    Clean up plan notebook

--- python/plan.py
+++ python/plan.py
@@ -8,7 +8,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.14.1
 #   kernelspec:
-#     display_name: Python 3.10.7 64-bit ('kurs_ffi')
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
